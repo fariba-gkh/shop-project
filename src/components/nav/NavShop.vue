2441b4ac744e0626b24ac58ff2ac5b2cb0f98dba
Fariba <fariba.gkh@gmail.com>
2023-03-12 12:47:09 -0700

    add feature dropdown

--- src/components/nav/NavShop.vue
+++ src/components/nav/NavShop.vue
@@ -1,43 +1,51 @@
 <template>
-    <!-- <div class="icon">
-       <i class="fa-solid fa-house"></i>
-       <font-awesome-icon icon="fa-solid fa-house" />
  
-    </div> -->
-    <div class="nav-shop">
+  <div class="nav-shop" >
     <nav>
-        <ul>
-            <li> <a href="">صفحه نخست</a> </li>
-            <li><a href="">برند</a> </li>
-            <li><a href="">لوازم جانبی</a> </li>
-            <li><a href="">گارانتی و خدمات</a> </li>
-            <li><a href="">وبلاگ</a> </li>
-            <li><a href="">درباره ما</a> </li>
-            <li><a href="">تماس با ما</a> </li>
-        </ul>
+      <ul>
+        <li><a href="">صفحه نخست</a></li>
+        <li>
+          <a href=""><drop-down-brand /></a>
+        </li>
+        <li>
+          <a href=""><drop-down-accessory /></a>
+        </li>
+        <li><a href="">گارانتی و خدمات</a></li>
+        <li><a href="">وبلاگ</a></li>
+        <li><a href="">درباره ما</a></li>
+        <li><a href="">تماس با ما</a></li>
+      </ul>
     </nav>
-    </div>
+  </div>
 </template>
 
 <script>
+import DropDownBrand from "../dropdown/DropDownBrand.vue";
+import DropDownAccessory from "../dropdown/DropDownAccessory.vue";
 export default {
-    name : 'NavShop',
-    
-}
+  name: "NavShop",
+  components: {
+    DropDownBrand,
+    DropDownAccessory,
+  },
+
+  
+};
 </script>
 <style>
-.nav-shop li{
-   list-style: none;
-   display: inline-block; 
-   padding: 10px 20px;
+.nav-shop li {
+  list-style: none;
+  display: inline-block;
+  padding: 10px 20px;
 }
 
 .nav-shop a {
-    text-decoration: none;
-    transition: all 0.3s ease 0s;
-    font-size: 14px;
+  text-decoration: none;
+  transition: all 0.3s ease 0s;
+  font-size: 14px;
+  color: rgb(19, 19, 19);
 }
 .nav-shop a:hover {
-    color: rgb(255, 166, 0);
+  color: rgb(255, 166, 0);
 }
 </style>
