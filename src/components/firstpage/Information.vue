16bbf3ab50d183aadf5b14e7c0228f753103d358
Fariba <fariba.gkh@gmail.com>
2023-03-16 12:16:51 -0700

    Edit style

--- src/components/firstpage/Information.vue
+++ src/components/firstpage/Information.vue
@@ -1,100 +1,109 @@
 <template>
-    <div class="container-inf">
-        <div class="carts-inf ">
-           <div class="cart-inf" v-for="(dataInf, id) in dataInformation" :key="id">
-            <div class="inf-contant">
-            <h3 class="inf-title">{{dataInf.title}}</h3>
-            <a href="">مشاهده</a>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor">
-    <path
-      d="M15 4a1 1 0 1 0 0 2V4zm0 11v-1a1 1 0 0 0-1 1h1zm0 4l-.707.707A1 1 0 0 0 16 19h-1zm-4-4l.707-.707A1 1 0 0 0 11 14v1zm-4.707-1.293a1 1 0 0 0-1.414 1.414l1.414-1.414zm-.707.707l-.707-.707.707.707zM9 11v-1a1 1 0 0 0-.707.293L9 11zm-4 0h1a1 1 0 0 0-1-1v1zm0 4H4a1 1 0 0 0 1.707.707L5 15zm10-9h2V4h-2v2zm2 0a1 1 0 0 1 1 1h2a3 3 0 0 0-3-3v2zm1 1v6h2V7h-2zm0 6a1 1 0 0 1-1 1v2a3 3 0 0 0 3-3h-2zm-1 1h-2v2h2v-2zm-3 1v4h2v-4h-2zm1.707 3.293l-4-4-1.414 1.414 4 4 1.414-1.414zM11 14H7v2h4v-2zm-4 0c-.276 0-.525-.111-.707-.293l-1.414 1.414C5.42 15.663 6.172 16 7 16v-2zm-.707 1.121l3.414-3.414-1.414-1.414-3.414 3.414 1.414 1.414zM9 12h4v-2H9v2zm4 0a3 3 0 0 0 3-3h-2a1 1 0 0 1-1 1v2zm3-3V3h-2v6h2zm0-6a3 3 0 0 0-3-3v2a1 1 0 0 1 1 1h2zm-3-3H3v2h10V0zM3 0a3 3 0 0 0-3 3h2a1 1 0 0 1 1-1V0zM0 3v6h2V3H0zm0 6a3 3 0 0 0 3 3v-2a1 1 0 0 1-1-1H0zm3 3h2v-2H3v2zm1-1v4h2v-4H4zm1.707 4.707l.586-.586-1.414-1.414-.586.586 1.414 1.414z"
-    />
-  </svg>
-           </div>
-           
+  <div class="container-inf">
+    <div class="carts-inf">
+      <div class="cart-inf" v-for="(dataInf, id) in dataInformation" :key="id">
+        <div class="inf-contant">
+          <h3 class="inf-title">{{ dataInf.title }}</h3>
+          <a href="">مشاهده</a>
         </div>
+        <div class="inf-svg">
+          <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="100px" height="100px"><path fill="#424242" d="M27,7h-6c-1.7,0-3,1.3-3,3v3h2v-3c0-0.6,0.4-1,1-1h6c0.6,0,1,0.4,1,1v3h2v-3C30,8.3,28.7,7,27,7z"/>
+            <path fill="#E65100" d="M40,43H8c-2.2,0-4-1.8-4-4V15c0-2.2,1.8-4,4-4h32c2.2,0,4,1.8,4,4v24C44,41.2,42.2,43,40,43z"/><path fill="#FF6E40" d="M40,28H8c-2.2,0-4-1.8-4-4v-9c0-2.2,1.8-4,4-4h32c2.2,0,4,1.8,4,4v9C44,26.2,42.2,28,40,28z"/><path fill="#FFF3E0" d="M26,26h-4c-0.6,0-1-0.4-1-1v-2c0-0.6,0.4-1,1-1h4c0.6,0,1,0.4,1,1v2C27,25.6,26.6,26,26,26z"/></svg>
+      
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import {ref} from 'vue';
+import { ref } from "vue";
 export default {
-    name: "Information",
+  name: "Information",
 
-    setup() {
-      const dataInformation = ref([
-        {
-            "id" : "0",
-          "title" : "مدارک مورد نیاز صدور سند",
-        },
-         {
-            "id" : "1",
-          "title" : "مدارک مورد نیاز صدور سند",
-        },
-         {
-            "id" : "2",
-          "title" : "مدارک مورد نیاز صدور سند",
-        },
-         {
-            "id" : "3",
-          "title" : "مدارک مورد نیاز صدور سند",
-        }
-      ])
+  setup() {
+    const dataInformation = ref([
+      {
+        id: "0",
+        title: "مدارک مورد نیاز صدور سند",
+      },
+      {
+        id: "1",
+        title: "مدارک مورد نیاز صدور سند",
+      },
+      {
+        id: "2",
+        title: "مدارک مورد نیاز صدور سند",
+      },
+      {
+        id: "3",
+        title: "مدارک مورد نیاز صدور سند",
+      },
+    ]);
 
     return {
-       dataInformation, 
-    }
- }
-}
+      dataInformation,
+    };
+  },
+};
 </script>
 <style>
 .container-inf {
-    background: rgba(185, 185, 185, 0.527);
-    height: 300px;
-    top: 60px;
-    margin: 50px 0;
+  background: rgba(185, 185, 185, 0.527);
+  height: 300px;
+  top: 60px;
+  margin: 50px 0;
 }
 .carts-inf {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap:40px ;
-    height: 100%;
-
-
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  height: 100%;
 }
 .cart-inf {
-     background-image:linear-gradient(to right, rgba(255, 217, 0, 0.801),  rgb(255, 255, 185)) ;
-     padding: 50px 40px;
-     border-radius: 5%;
-     display: flex;
-     flex-direction: row;
-     justify-content: center;
-     align-items: center;
+  background-image: linear-gradient(
+    to right,
+    rgba(255, 217, 0, 0.801),
+    rgb(255, 255, 185)
+  );
+  padding: 40px 40px;
+  border-radius: 5%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
 }
-
-.cart-inf svg {
-margin: auto;
+.cart-inf:hover{
+  box-shadow: -3px 3px 8px rgba(145, 99, 1, 0.486);
+}
+.inf-svg {
+  width: 40%;
 }
 .inf-contant {
-    width: 70%;
+  width: 60%;
+  padding: 0 10px;
 }
 .inf-contant h3 {
-font-size: 1rem;
-margin-bottom: 10px;
-font-weight: 500;
-color: rgb(77, 77, 77);
-
+  font-size: 1rem;
+  margin-bottom: 10px;
+  font-weight: 600;
+  color: rgb(77, 77, 77);
 }
 .inf-contant a {
-border: solid 2px rgba(221, 221, 221, 0.74);
-padding: 2px 5px ;
-font-size: 0.7rem;
-color: rgb(58, 58, 58);
-font-weight: 400;
-border-radius: 5%;
-margin-top: 50px;
-
+  border: solid 2px  rgba(184, 184, 184, 0.74);
+  padding: 2px 10px;
+  font-size: 0.8rem;
+  color: rgb(97, 97, 97);
+  font-weight: 500;
+  border-radius: 10%;
+  margin-top: 50px;
+  text-decoration: none;
+  transition: all 0.5s;
+}
+.inf-contant a:hover {
+  background-color: rgba(184, 184, 184, 0.74);
+  color: rgb(20, 20, 20);
 }
 </style>
