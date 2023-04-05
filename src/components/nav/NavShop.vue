210bd10af0ce81230a0b57298b4cc9f8a28b0085
Fariba <fariba.gkh@gmail.com>
2023-04-04 17:50:10 -0700

    add fixed position & active class

--- src/components/nav/NavShop.vue
+++ src/components/nav/NavShop.vue
@@ -1,30 +1,35 @@
 <template>
- 
+ <div class="nav-container">
   <div class="nav-shop" >
     <nav>
       <ul>
-        <li><router-link :to="{name:'home'}">صفحه نخست</router-link></li>
-        <!-- <li><a href="">صفحه نخست</a></li> -->
+        <li @click="activePage = 'firstPage'" :class="{active:activePage === 'firstPage'}">
+          <router-link :to="{name:'home'}">صفحه نخست</router-link></li>
         <li>
           <a href=""><drop-down-brand /></a>
         </li>
         <li>
           <a href=""><drop-down-accessory /></a>
         </li>
-        <li><a href="">گارانتی و خدمات </a></li>
-        <li><a href="">وبلاگ</a></li>
-        <li><a href="">درباره ما</a></li>
-        <li><router-link :to="{name:'ContactUs'}">تماس با ما</router-link></li>
-        <!-- <li><a href="">تماس با ما</a></li> -->
+        <li @click="activePage = 'service'" :class="{active: activePage === 'service'}">
+          <router-link :to="{name:'service'}">گارانتی و خدمات </router-link></li>
+        <li @click="activePage = 'blog'" :class="{active:activePage === 'blog'}">
+          <router-link :to="{name: 'blog'}" >وبلاگ</router-link></li>
+        <li @click="activePage = 'aboutUs'" :class="{active:activePage === 'aboutUs'}">
+          <router-link :to="{name:'aboutUs'}">درباره ما</router-link></li>
+        <li  @click="activePage = 'contactUs'" :class="{active:activePage === 'contactUs'}">
+          <router-link :to="{name:'ContactUs'}">تماس با ما</router-link></li>
       </ul>
     </nav>
   </div>
+</div>
 </template>
 
 <script>
 import { RouterLink } from "vue-router";
 import DropDownBrand from "../dropdown/DropDownBrand.vue";
 import DropDownAccessory from "../dropdown/DropDownAccessory.vue";
+import { ref } from "vue";
 export default {
   name: "NavShop",
   components: {
@@ -32,18 +37,18 @@ export default {
     DropDownAccessory,
     RouterLink
   },
-
+setup(){
+  const activePage = ref(null);
+  return {
+    activePage
+  }
+}
   
 };
 </script>
 <style>
 
-.nav-shop{
-  position: fixed;
-  z-index: 10000;
-  width: 100%;
-  margin: 40px;
-}
+
 .nav-shop li {
   list-style: none;
   display: inline-block;
@@ -63,5 +68,19 @@ export default {
   background: white;
     padding:0 20px;
     border-bottom: 0.7px solid rgba(218, 218, 218, 0.637);
+    position: fixed;
+  width: 100%;
+  z-index: 10000;
+  margin-top: 61px;
+}
+.nav-container {
+  height: 100px;
+}
+
+.nav-shop .active{
+  background: white;
+}
+.active a{
+  color: rgb(255, 166, 0);
 }
 </style>
