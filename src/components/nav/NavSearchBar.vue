2368ea91785a1fcf7e585232e179cb7843af5e2e
Fariba <fariba.gkh@gmail.com>
2023-04-04 17:47:47 -0700

    add new style

--- src/components/nav/NavSearchBar.vue
+++ src/components/nav/NavSearchBar.vue
@@ -1,5 +1,4 @@
 <template>
-
     <div class="nav-search-bar">
         <div class="row">
          <div class="logo">
@@ -7,15 +6,11 @@
             <span>HELMET</span>
         </div>
          <div class="search-box">
-            <!-- <form action=""> -->
-                <!-- <button class="search-icon"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></button> -->
                 <input class="input-search" size="number" type="text" placeholder="جستجو کنید...">
                 <button type="submite" class="search-btn">
-                    <!-- <i class="fal fa-search"></i> -->
                     <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="20px" height="20px"><path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"/></svg>
 
                 </button>
-            <!-- </form> -->
         </div>
         </div>
         <div class="row">
@@ -79,13 +74,18 @@ setup() {
     justify-content: space-between;
     flex-wrap: wrap;
     border-bottom: 1px solid rgba(223, 223, 223, 0.596);
-    
+    background: white;
+    padding:0 20px;
+    position: fixed;
+    width: 100%;
+    z-index: 10000;
 }
+
 .row {
     display: flex;
     align-items: center;
     flex-wrap: wrap;
-
+    padding:0 20px;
 }
 .register {
  display: flex;
