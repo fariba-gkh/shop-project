e0c8818d5502d2dc7a0568c64caad5ef1a2c5258
Fariba <fariba.gkh@gmail.com>
2023-04-26 08:31:22 -0700

    add new component

---
+++ src/components/property/ProductProperty.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>lorem</h1>
+    </div>
+</template>
+<script>
+export default {
+    setup(props) {
+        props: {
+    discountData : Array;
+}
+    }
+}
+
+</script>
+<style>
+</style>
